e396ea3b43cf3e13ec495c38b9425157abda2ac7
Leonardo Soares Santos <leonardo_soares_santos@outlook.com>
2019-09-26 02:23:16 -0300

    Initial commit

---
+++ app/src/main/java/leonardo/soares/mvvmwithkotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package leonardo.soares.mvvmwithkotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
